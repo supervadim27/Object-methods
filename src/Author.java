f278a08289c9aec57553d33637ba94a4d0a9b558
Вадим <va.arutyunov@gmail.com>
2023-12-07 14:13:52 +0300

    done homework

--- src/Author.java
+++ src/Author.java
@@ -1,16 +1,33 @@
 public class Author {
-    private String name;
-    private String surname;
-
-    public Author(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-
+    public String firstName;
+    public String lastName;
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
     public String getName() {
-        return name;
+        return firstName;
     }
     public String getSurname() {
-        return surname;
+        return lastName;
+    }
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Author)) {
+            return false;
+        }
+        Author author = (Author) obj;
+        return firstName.equals(author.firstName) && lastName.equals(author.lastName);
+    }
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        return result;
     }
 }
